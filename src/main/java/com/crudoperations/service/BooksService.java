5a374d775374397ea49fcd40bd781431ce92cf51
shashank01sj <shashank@sportjacks.com>
2024-07-15 17:04:43 +0530

    changes

--- src/main/java/com/crudoperations/service/BooksService.java
+++ src/main/java/com/crudoperations/service/BooksService.java
@@ -1,13 +1,45 @@
 package com.crudoperations.service;
 
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.ArrayList;  
-import java.util.List;  
-import org.springframework.beans.factory.annotation.Autowired;  
-import org.springframework.stereotype.Service;  
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.crudoperations.model.Books;
 import com.crudoperations.repository.BooksRepository;
 
+//mark the class as a Service
+@Service
 public class BooksService {
+    @Autowired
+    BooksRepository booksRepository;
+
+
+    //getting all books record by using the method findaAll() of CrudRepository
+
+    public List<Books> getAllBooks() {
+        List<Books> books = new ArrayList<Books>();
+        booksRepository.findAll().forEach(books1 -> books.add(books1));
+        return books;
+    }
     
+    //getting a specific record by using the method findById() of CrudRepository
+    public Books getBooksById(int id) {
+        return booksRepository.findById(id).get();
+    }
+
+    //saving a specific record by using the method save() of CrudRepository
+    public void saveOrUpdate(Books books) {
+        booksRepository.save(books);
+    }
+
+    //delete a specific record by using deletebyId() method of CrudRepository
+    public void delete(int id) {
+        booksRepository.deleteById(id);
+    }
+
+    //updating a record 
+    public void update(Books books, int bookid){
+        booksRepository.save(books);
+    }
 }
