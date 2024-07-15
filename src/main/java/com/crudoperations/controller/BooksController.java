d423c63cc6fb57b40c0a78ac1a5978e5d01f9f84
shashank01sj <shashank@sportjacks.com>
2024-07-15 17:10:10 +0530

    changes

--- src/main/java/com/crudoperations/controller/BooksController.java
+++ src/main/java/com/crudoperations/controller/BooksController.java
@@ -1,15 +1,16 @@
 package com.crudoperations.controller;
 
-import java.util.*;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.crudoperations.model.Books;
 import com.crudoperations.service.BooksService;
 
@@ -38,7 +39,7 @@ public class BooksController {
         booksService.delete(bookid);
     }
 
-    //creating post mapping that post the book detail in the database
+    //creating post mapping that post the book detail heytesting in the database
     @PostMapping("/books")
     private int saveBook(@RequestBody Books books){
         booksService.saveOrUpdate(books);
